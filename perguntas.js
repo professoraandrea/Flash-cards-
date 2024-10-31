aec8335f8b0938c16b873b64116e7be31755cd9e
Andrea <andrea.oliveira.souza@escola.pr.gov.br>
2024-10-31 17:46:07 -0300

    alteracao flaschcards

---
+++ perguntas.js
@@ -0,0 +1,11 @@
+criaCartao{
+    " Programação"
+    "O que é Java Script?"
+    "O Java Script é uma linguagem de programação."
+}
+
+criaCartao{
+    "O que é CSS?"
+    "O CSS é uma linguagem de estilização"
+}
+
