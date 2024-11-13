0b3de246b1ec2b203905e104226bd6eb8f345642
Andrea <andrea.oliveira.souza@escola.pr.gov.br>
2024-11-13 17:41:05 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,11 +1,17 @@
-criaCartao{
-    " Programação"
-    "O que é Java Script?"
-    "O Java Script é uma linguagem de programação."
-}
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
 
-criaCartao{
-    "O que é CSS?"
-    "O CSS é uma linguagem de estilização"
-}
+criaCartao(
+    'Programação',
+    'O que é Java Script?',
+    'É uma linguagem de programação interpretada de alto nível'
+)
 
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
