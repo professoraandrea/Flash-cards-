5d1d7c3d96469716c0aa813aaaef0915b0225732
Andrea <andrea.oliveira.souza@escola.pr.gov.br>
2024-11-14 16:45:25 -0300

    alteração

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 function criaCartao(categoria, pergunta, resposta){
-    console.log(categoria, pergunta, resposta);
+    //console.log(categoria, pergunta, resposta);
+
+let cartao=document.createElement ("article");
+cartao.className= "cartao";
+cartao.innerHTML= `
+            <div class="cartao__conteudo">
+                <h3>${categoria}</h3>
+                <div class="cartao_conteudo_pergunta">
+                    <p>${pergunta}</p>
+                </div>
+                <div class="cartao_conteudo_resposta">
+                    <p>${resposta}</p>
+                </div>
+            </div>
+`
+let container= document.getElementById('container');
+container.appendChild(cartao);
 }
